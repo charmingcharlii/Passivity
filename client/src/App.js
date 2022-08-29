3c0d98612b72bb27b8f17398383b9851ee062f41
TWetmore34 <tjwetmore34@gmail.com>
2022-08-29 13:23:55 -0400

    merge fixes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <Routes>
         {/* landing route */}
@@ -29,24 +28,6 @@ function App() {
         } />
       </Routes>
     </Router>
-
-=======
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> d33c4868ff37815933579b4c80152762c2b9649e
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-grey">
+        testing
+    </header>
+  )
+}
+
+export default Header
