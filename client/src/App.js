3c25dde5107b8852054e40ce9613f4a2b064af5b
TWetmore34 <tjwetmore34@gmail.com>
2022-08-29 12:47:07 -0400

    added routing to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* landing route */}
+        <Route path='/' element={
+          <div>
+            <p>home</p>
+          </div>
+        } />
+
+        {/* about page */}
+        <Route path='/about' element={
+          <div>
+            <p>about</p>
+          </div>
+        } />
+
+        {/* portfolio route */}
+        <Route path='/portfolio' element={
+          <div>
+            <p>portfolio</p>
+          </div>
+        } />
+      </Routes>
+    </Router>
+
   );
 }
 
