22bd0bf18f788bfebd461ba66c2bf84b4de25724
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-08-29 18:26:57 -0400

    Adding about section

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,26 +1,50 @@
 const About = () => {
-    // if we need to repeat any more of these, set them as another component and render them w the proper styles based on % 2 === 0 for even odd
-  return (
-    <div className="p-20">
-        <h2 className="text-center text-3xl font-semibold mx-4 mb-4">About us</h2>
-        <hr></hr>
-            <div className="flex m-8">
-                <img className="grow mr-4" src="https://picsum.photos/150/100" alt="placeholder"></img>
-                <div className="w-1/2">
-                    <h2 className="text-center text-2xl font-semibold mx-4 mb-4">some header here</h2>
-                    <p>Some other more descriptive text down here that tells our reader all about the benifits of our amazing thing</p>
-                </div>
-                </div>
-                <hr></hr>
-            <div className="flex flex-row-reverse m-8">
-                <img className="grow ml-4" src="https://picsum.photos/150/100" alt="placeholder"></img>
-                <div className="w-1/2">
-                    <h2 className="text-center text-2xl font-semibold mx-4 mb-4">some header here</h2>
-                    <p>Some other more descriptive text down here that tells our reader all about the benifits of our amazing thing</p>
-                </div>
-            </div>
-        </div>
-  )
-}
-
-export default About
+    return (
+      <div class="flex items-center justify-center h-screen">
+      
+      <div class="bg-slate-200 text-white font-bold rounded-lg border shadow-lg p-10">
+  
+  <div class="p-4 text-gray-600">
+      <h1 class="mb-8 text-center text-3xl font-bold text-indigo-900">This is - what we do :</h1>
+  
+      <ul class="grid place-content-center sm:grid-cols-2 gap-8">
+          <li class="flex">
+              <div class="px-4 text-5xl font-extralight text-indigo-700">01.</div>
+              <div>
+                  <div class="text-xl font-bold text-indigo-800">Why?</div>
+                  <p class="max-w-xs py-2 font-light text-sm text-indigo-900">We know investing can sometimes feel like a really hard 🧩. Here at passivity we feel that, innvesting doesn’t have to be that hard!</p>
+              </div>
+          </li>
+          <li class="flex">
+              <div class="px-4 text-5xl font-extralight text-indigo-700">02.</div>
+              <div>
+                  <div class="text-xl font-bold text-indigo-800">Strategy</div>
+                  <p class="max-w-xs py-2 font-light text-sm text-indigo-900">With thousands of stocks to choose from, and a million and 1 investing strategies out there our goal is to help you sift through the data and find, organize, and track which companies match your dividend investment style, and add value 💸 to you and your future.</p>
+              </div>
+          </li>
+          <li class="flex">
+              <div class="px-4 text-5xl font-extralight text-indigo-700">03.</div>
+              <div>
+                  <div class="text-xl font-bold text-indigo-800">Design</div>
+                  <p class="max-w-xs py-2 font-light text-sm text-indigo-900"> Analysis paralysis is a real thing - we took the data 🧩, and made it easy to 📘 by designing an easy access table that will give you all the information you need to get your portfolio started, and nothing you don't.
+                  </p>
+              </div>
+          </li>
+          <li class="flex">
+              <div class="px-4 text-5xl font-extralight text-indigo-700">04.</div>
+              <div>
+                  <div class="text-xl font-bold text-indigo-800">Development</div>
+                  <p class="max-w-xs py-2 font-light text-sm text-indigo-900">We build the right solution for your business. We
+                      craft beautiful, intuitive, and user-friendly interfaces that are easy to use and easy to maintain.
+                  </p>
+              </div>
+          </li>
+      </ul>
+  </div>
+      </div>
+      
+    </div>
+    )
+  }
+  
+  export default About
