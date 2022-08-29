5495b468bec2088d9c1c29d693c0d6fbb25d5369
TWetmore34 <tjwetmore34@gmail.com>
2022-08-29 14:04:51 -0400

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header';
+
 
 function App() {
   return (
     <Router>
+        <Header />
       <Routes>
         {/* landing route */}
         <Route path='/' element={
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <header className="bg-grey">
-        testing
+    <header className="bg-slate-50 h-16 w-full fixed">
+            <h1 className="text-5xl font-extrabold m-2 inline">Passivity</h1>
+        <nav className="inline-flex fixed right-0">
+                <Link to="/about" className='w-fit m-4'>About</Link>
+                <Link to="/portfolio" className='w-fit m-4'>My Portfolio</Link>
+        </nav>
     </header>
   )
 }
