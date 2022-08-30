6dd113e871d478b4526c084c00069dcb771f092f
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-08-30 09:37:06 -0400

    refining home sction

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div class="flex items-center justify-center h-screen">
-            <div class="py-20 bg-stone-100 my-20">
+            <div class="transform md:rounded-md py-20 bg-stone-100 my-20">
                 <div class="grid grid-cols-1 sm:grid-cols-2  gap-10 mx-auto  items-center max-w-6xl mx-auto">
                     <div class="px-4 md:mr-6">
                         <h3 class="text-4xl pr-6 sm:leading-snug tracking-tight font-bold text-black">
@@ -12,20 +12,10 @@ const Home = () => {
                         </p>
                     </div>
                     <div>
-                        <div class="absolute bg-slate-300 transform -translate-x-10 relative h-64">
+                        <div class="transform md:rounded-md absolute bg-slate-300 transform -translate-x-10 relative h-64">
                         </div>
                         <div class="transform md:rounded-md  bg-white rotate-3 scale-110 translate-x-10 md:shadow-2xl -ml-4 -mt-44 p-12  space-y-2">
                             <div class="px-4 sm:px-0 grid grid-cols-5 sm:grid-cols-4 gap-2 sm:gap-8 max-w-5xl mx-auto">
-                                {/* <img src="https://cdn.mjwebs.com/img-neodymium/lenders/1.png"> </img>
-                            <img src="https://cdn.mjwebs.com/img-neodymium/lenders/2.png"> </img>
-                            <img src="https://cdn.mjwebs.com/img-neodymium/lenders/3.png"> </img>
-                            <img src="https://cdn.mjwebs.com/img-neodymium/lenders/4.png"> </img>
-                            <img src="https://cdn.mjwebs.com/img-neodymium/lenders/5.png"> </img>
-                            <img src="https://cdn.mjwebs.com/img-neodymium/lenders/6.png"> </img>
-                            <img src="https://cdn.mjwebs.com/img-neodymium/lenders/7.png"> </img>
-                            <img src="https://cdn.mjwebs.com/img-neodymium/lenders/8.png"> </img>
-                            <img src="https://cdn.mjwebs.com/img-neodymium/lenders/9.png"> </img>
-                            <img src="https://cdn.mjwebs.com/img-neodymium/lenders/10.png"> </img> */}
                             </div>
                         </div>
                     </div>
