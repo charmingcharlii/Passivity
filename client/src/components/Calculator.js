888e83bdab140b061fa1958d1f4a0c94df956bdc
victoriaDunlap <victoriacdunlap@gmail.com>
2022-08-29 21:44:46 -0400

    added some bones for the calculator

--- client/src/components/Calculator.js
+++ client/src/components/Calculator.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react';
+// import '../'
 
 export default function Calculator() {
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    //         'X-RapidAPI-Host': 'fidelity-investments.p.rapidapi.com'
+    //     }
+    // };
+    // const autoComplete = 
+    // fetch('https://fidelity-investments.p.rapidapi.com/v2/auto-complete?q=apple', options)
+    //     .then(response => response.json())
+    //     .then(response => console.log(response))
+    //     .catch(err => console.error(err));
 
   return (
-    <div>Calculator</div>
+    <div>
+        <table id="table_id" class="display">
+            <thead>
+                <tr>
+                    <th>Stock</th>
+                    <th>Column 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Row 1 Data 1</td>
+                    <td>Row 1 Data 2</td>
+                </tr>
+                <tr>
+                    {/* <td>{autoComplete}</td> */}
+                    <td>Row 2 Data 2</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
   )
 }
