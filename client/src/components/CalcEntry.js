7e4725884ab74da9c6b7c5afe1353999f68ef528
victoriaDunlap <victoriacdunlap@gmail.com>
2022-09-01 11:01:05 -0400

    Calculator is calculating. Removed Home.js & Calculator.js

--- client/src/components/CalcEntry.js
+++ client/src/components/CalcEntry.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react';
 const CalcEntry = ({ symbol, holding }) => {
   const [avgStockPrice, setAvgStockPrice] = useState(0)
-  const [base, setBase] = useState(0)
+  const [estimatedReturn, setEstimatedReturn] = useState(0)
+  const [holdings, setHoldings] = useState(0)
+  const [stock, setStock] = useState('')
+  // const [dividends, setDividends] = useState(0)
+
   // creates the fetch call from user input 
-  let userInput = 'AAPL' // will be user input 
-  let fetchCall = `https://fidelity-investments.p.rapidapi.com/quotes/get-mashup?symbol=${userInput}`
+  // let userInput = {stock} // will be user input 
+  let fetchCall = `https://fidelity-investments.p.rapidapi.com/quotes/get-mashup?symbol=${stock}`
 
   const options = {
     method: 'GET',
@@ -31,7 +35,7 @@ const CalcEntry = ({ symbol, holding }) => {
             // data from api call
             let averageStockPrice = testData.mashup?.["dividend"]?.dividendDetails.dividendDetail[0].equityDetail.payoutRatioTTM
             setAvgStockPrice(averageStockPrice)
-            setBase(userInputShares * averageStockPrice)
+            // setestimatedReturn(userInputShares * averageStockPrice)
             console.log('finished!')
           }
         }
@@ -42,20 +46,28 @@ const CalcEntry = ({ symbol, holding }) => {
   }
 
   const runFetchResponse = async () => {
-    return await getFetchResponse(fetchCall, 5)
+    return await getFetchResponse(fetchCall, holdings)
       .then(() => {
         console.log("avgStonk", avgStockPrice)
-        console.log('base', base)
+        console.log('estimatedReturn', estimatedReturn)
+        console.log("hold", holdings)
       })
   }
-
-
+  
+  const calculate = () => {
+    setEstimatedReturn(holdings * avgStockPrice)
+   return (
+    console.log({estimatedReturn})
+   )
+  }
 
   return (
     <div className="border-black border-2 w-full h-auto flex justify-between">
-      <p className="grow">Stock Price Avg: <span className='output'>{avgStockPrice === 0 ? 'Loading...' : `$${avgStockPrice.toFixed(2)}`}</span></p>   {/* will bxe userInput? */}
-      <p className="grow">Base: <span className='output'>{base === 0 ? 'Loading...' : `$${base.toFixed(2)}`}</span></p> {/* will be shares? */}
-      <button className="grow">Edit</button>
+      <p className="grow">Stock: <input onChange={(e) => { setStock(e.target.value) }} value={stock} type="text"></input></p>
+      <p className="grow">Holdings: <input onChange={(e) => { setHoldings(e.target.value) }} value={holdings} type="integer"></input></p>
+      <p className="grow">Avg Stock Price: <span className='output'>{avgStockPrice === 0 ? 'Loading...' : `$${avgStockPrice.toFixed(2)}`}</span></p> 
+      <p className="grow">Estimated Return: <span className='output'>{estimatedReturn === 0 ? 'Loading...' : `$${estimatedReturn.toFixed(2)}`}</span></p> 
+      <button className="grow" onClick={calculate}>Calculate</button>
 
     </div>
   )
