3b1b155313f30f14756a690a74eaca98a841110a
chromodyne <dsinitiere@outlook.com>
2022-08-31 17:32:11 -0700

    Minor fixes and documentation improved.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,12 @@
+//Imports
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Stocks } = require('../models/');
 const { signToken } = require("../utils/auth");
-// will need to import auth and User model 
 
 const resolvers = {
     Query: {
+
+        //Query a user's infomation.
         me: async (parent, args, context) => {
             if(context.user) {
                 return User.findById(context.user._id).populate('userPortfolio')
@@ -14,6 +16,7 @@ const resolvers = {
 
     Mutation: {
 
+        //Used for adding a new user to the database.
         addUser: async (parent, args) => {
             const user = await User.create(args);
 
@@ -23,28 +26,36 @@ const resolvers = {
 
         }, 
 
+        //Used for logging in to an existing account.
         login: async (parent, { username, password }) => {
             const user = await User.findOne({ username });
 
+            //If the user DNE then throw an auth error.
             if(!user) {
                 throw new AuthenticationError('No user with this login found.')
             }
 
+            //Check the user's password against the encrypted password in DB.
             const passwordAuth = await user.checkPassword(password);
 
+            //If the password check fails throw an error.
             if(!passwordAuth) {
                 throw new AuthenticationError('Incorrect Password.')
             }
+            
+            //Sign a new JWT on login.
             const token = signToken(user);
             
             return {token, user};
 
         },
 
+        //Used for updating an existing user's account information.
         updateUser: async (parent, {username, email, password}, context) => {
             
             if (context.user) {
 
+                //Find a user by the id parameter passed in then update the necessary fields.
                 const user = await User.findOneAndUpdate(
                     { _id: context.user._id },
                     { email: email, username: username, password: password },
@@ -55,20 +66,25 @@ const resolvers = {
 
             }
 
+            //Throw an auth error if the user is not logged in.
             throw new AuthenticationError("You must be logged in.");
 
         },
 
+        //Used for saving a row to the user's portfolio.
         saveHolding: async (parent, {holdingData}, context) => {
 
 
             if(context.user) {
 
+                //Create a stock variable with the passed in parameters.
                 const stock = ({
                             ticker: holdingData.ticker,
                             holding: holdingData.holding,
                             value: holdingData.value
                 })
+
+                //Update the user's portfolio array with the new data.
                 const updatedPortfolio = User.findOneAndUpdate(
                     { _id: context.user._id },
                     {$push: {
@@ -85,15 +101,17 @@ const resolvers = {
 
         },
 
-        //Used for removing 
+        //Used for removing a row from the user's portfolio.
         removeHolding: async (parent, ticker, context) => {
 
             if(context.user) {
 
+                //Find a stock using the passed in ticker parameter.
                 const stock = await Stocks.findOne(
                     {ticker: ticker}
                 )
-
+                
+                //Pop the stock with that ticker off the array.
                 const updateUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
                     { $pop: { userPortfolio: stock }},
