539a463c6e05ba7bec80a9959d5bd17f0bc18e3d
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-09-01 20:53:40 -0400

    updating team section socials

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full h-16 bg-indigo-600 border-t-2 border-white fixed left-0 bottom-0 flex justify-center items-center text-white text-2xl">
+    <footer className="w-full h-16 bg-gray-800 border-t-2 border-white fixed left-0 bottom-0 flex justify-center items-center text-white text-2xl">
       <div className="text-center">
         <p>
           © Copyright 2022 <a className="font-semibold" href="mailto:m.Group3@gmail.com"
